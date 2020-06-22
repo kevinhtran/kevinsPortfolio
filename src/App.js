76a2060f0421e067c1c5338cb916121d0d7e7e16
kevinhtran <kevdeveloper7@gmail.com>
2020-06-21 23:59:00 -0700

    installed react router

--- src/App.js
+++ src/App.js
@@ -41,3 +41,4 @@ export default App;
 // step 1: Convert the app.js component into a class base component because we will be using state in here
 // step 2: make a constructor to get props and super() to take in those props. have the state for the top level stuff on the website
 // state is for the title that will be in the nav bar/header, state for the link that is inside of the navbar, state for the home/about/contact page but only the title of these pages
+// step 3: in the terminal, run this command for the react router: 'npm install --save -react-router-dom' then 'npm run start'
